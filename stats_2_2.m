77aee426cbb94b69776eaa43a8a8fbede3420555
CyrilAntille <cyrilantille@gmail.com>
2016-09-07 15:19:03 +0200

    stats_2.2 plots re-implemented

--- stats_2_2.m
+++ stats_2_2.m
@@ -8,18 +8,14 @@ mainP.num_beams = 101; % can be a single value or list of values
 mainP.shift_per_beam = true;
 
 speeds = 0:1:2;
-all_3dB_pts = cell(size(speeds));
-all_3dB_inters = cell(size(speeds));
+pts_3dB_width = zeros([length(mainP.pts_range), length(mainP.num_beams),...
+    length(mainP.methods_set), length(speeds)]);
 for sp=1:length(speeds)
     mainP.shift = Shift(ShiftType.LateralSpeed, speeds(sp), -1);
     main_init
 
     %3dB width computations
     max_peak_DAS = 0;
-    pts_3dB = cell([length(mainP.pts_range), ...
-        length(mainP.methods_set), length(mainP.num_beams)]);
-    inters_3dB = cell([length(mainP.pts_range), ...
-        length(mainP.methods_set), length(mainP.num_beams)]);
     for m=1:length(mainP.methods_set)
         m_BF = data_BF{m};
         for b=1:length(mainP.num_beams)
@@ -52,127 +48,30 @@ for sp=1:length(speeds)
                 else
                     p_width = p_x(2) - p_x(1);
                 end
-                pts_3dB{p,m,b} = [pdb(1), p_width];
-                inters_3dB{p,m,b} = p_x;
+                pts_3dB_width(p,b,m,sp) = p_width;
             end
         end
     end
-    all_3dB_pts{sp} = pts_3dB;
-    all_3dB_inters{sp} = inters_3dB;
-    clearvars -except all_3dB_pts all_3dB_inters mainP speeds
+    clearvars -except mainP speeds pts_3dB_width
 end
 
 %% Plots
 figure;
 linestyle_list = {':','-','--','-.','-'};
 markers_list = {'+','x','diamond','o','*'};
-for b=1:length(num_beams)
-    fprintf('\nNum Beams: %d.\n', num_beams(b))
-    for p=1:length(mainP.pts_theta)
-        num_m = size(mainlobes_3dBwidth{1},1);
-
-        p_raw = zeros(length(speeds), num_m);
-        for s=1:length(speeds)
-            for m=1:num_m
-                p_raw(s,m) = mainlobes_3dBwidth{s}{m,b}(p);
-            end
-        end
-        subplot(2,1,1)
-        p1 = plot(speeds, p_raw, 'LineWidth', 2);
+for p=1:size(pts_3dB_width, 1)
+    for b=1:size(pts_3dB_width, 2)
+        p1 = plot(speeds, squeeze(pts_3dB_width(p,b,:,:)), 'LineWidth', 2);
         for pidx=1:length(p1)
             p1(pidx).Marker = markers_list{pidx};
             p1(pidx).LineStyle = linestyle_list{pidx};
         end
-        legend({'DAS','MV', 'IAA-MBMB','IAA-MBMB-Upsampled'}, 'Location', 'best')
+        legend(mainP.methods_set, 'Location', 'best')
         title('Beampattern mainlobes 3dB width')
         xlabel('Speed [m/s]')
         ylabel('Mainlobe 3dB width [degrees]')
-
-        b_rel = p_raw;
-        for m=1:num_m
-            b_rel(:,m) = b_rel(:,m) ./ b_rel(1,m) * 100 - 100;
-        end
-        subplot(2,1,2)
-        p2 = plot(speeds, b_rel, 'LineWidth', 2);
-        for pidx=1:length(p2)
-            p2(pidx).Marker = markers_list{pidx};
-            p2(pidx).LineStyle = linestyle_list{pidx};
-        end
-        legend({'DAS','MV', 'IAA-MBMB','IAA-MBMB-Upsampled'}, 'Location', 'best')
-        title('Mainlobes width relative increase')
-        xlabel('Speed [m/s]')
-        ylabel('Mainlobe width increase [%]')
-        if save_plots
-            p_range = round(data_phantoms.positions(p,3) * 1000);
-            im_name = strcat('stats_', int2str(num_beams(b)), '_', ...
-                int2str(p_range), '_', num2str(speeds(1), 2), ...
-                '_', num2str(speeds(end), 2));
-            saveas(gcf, strcat('../images/fig/', im_name, '.fig'), 'fig')
-            saveas(gcf, strcat('../images/png/', im_name, '.png'), 'png')
-        end
-        if show_plots
-            pause
-        end
+        pause
     end
 end
 close
-
-%% 
-if show_plots
-    figure('units','normalized','position',[.2 .3 .5 .5])
-else
-    figure('units','normalized','position',[.2 .3 .5 .3],'Visible','off')
-end
-
-for b=1:length(num_beams)
-    fprintf('\nNum Beams: %d.\n', num_beams(b))
-    for p=1:2
-        num_m = size(mainlobes_3dBwidth{1},1);
-
-        p_raw = zeros(length(speeds), num_m);
-        for s=1:length(speeds)
-            for m=1:num_m
-                p_raw(s,m) = mainlobes_peak{s}{m,b}(p);
-            end
-        end
-        subplot(2,1,1)
-        p1 = plot(speeds, p_raw, 'LineWidth', 2);
-        for pidx=1:length(p1)
-            p1(pidx).Marker = markers_list{pidx};
-            p1(pidx).LineStyle = linestyle_list{pidx};
-        end
-        legend({'DAS','MV', 'IAA-MBMB','IAA-MBMB-Upsampled'}, 'Location', 'best')
-        title('Peak amplitude')
-        xlabel('Speed [m/s]')
-        ylabel('Peak amplitude [dB]')
-
-        b_rel = p_raw;
-        for m=1:num_m
-            b_rel(:,m) = b_rel(:,m) ./ b_rel(1,m) * 100 - 100;
-        end
-        subplot(2,1,2)
-        p2 = plot(speeds, b_rel, 'LineWidth', 2);
-        for pidx=1:length(p2)
-            p2(pidx).Marker = markers_list{pidx};
-            p2(pidx).LineStyle = linestyle_list{pidx};
-        end
-        legend({'DAS','MV', 'IAA-MBMB','IAA-MBMB-Upsampled'}, 'Location', 'best')
-        title('Mainlobes width relative increase')
-        xlabel('Speed [m/s]')
-        ylabel('Peak amplitude increase [%]')
-        if save_plots
-            p_range = round(data_phantoms.positions(p,3) * 1000);
-            im_name = strcat('maxPeaks_', int2str(num_beams(b)), '_', ...
-                int2str(p_range), '_', num2str(speeds(1), 2), ...
-                '_', num2str(speeds(end), 2));
-            saveas(gcf, strcat('../images/fig/', im_name, '.fig'), 'fig')
-            saveas(gcf, strcat('../images/png/', im_name, '.png'), 'png')
-        end
-        if show_plots
-            pause
-        end
-    end
-end
-close
-
 fprintf('Stats_2_2 finished!')
